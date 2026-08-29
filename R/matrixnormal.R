bb7355ef0c2cfbe6e14fb5ef257722d5f171214b
Polarolouis <Polarolouis@users.noreply.github.com>
2026-08-29 07:30:38 +0000

    Style code (GHA)

--- R/matrixnormal.R
+++ R/matrixnormal.R
@@ -8,25 +8,25 @@
 #'
 #' @returns the density a scalar
 dmatrixnormal_plain <- function(X, M, U, V, log_p = FALSE) {
-    n <- nrow(X)
-    p <- ncol(X)
+  n <- nrow(X)
+  p <- ncol(X)
 
-    dev <- X - M
-    U_inv <- solve(U)
-    V_inv <- solve(V)
-    det_U <- det(U)
-    det_V <- det(V)
+  dev <- X - M
+  U_inv <- solve(U)
+  V_inv <- solve(V)
+  det_U <- det(U)
+  det_V <- det(V)
 
-    exponent <- -0.5 * sum(diag(V_inv %*% t(dev) %*% U_inv %*% dev))
+  exponent <- -0.5 * sum(diag(V_inv %*% t(dev) %*% U_inv %*% dev))
 
-    log_norm_const <- -0.5 * (n * p * log(2 * pi) + n * log(det_V) + p * log(det_U))
+  log_norm_const <- -0.5 * (n * p * log(2 * pi) + n * log(det_V) + p * log(det_U))
 
-    if (log_p) {
-        out <- log_norm_const + exponent
-    } else {
-        out <- exp(log_norm_const + exponent)
-    }
-    return(out)
+  if (log_p) {
+    out <- log_norm_const + exponent
+  } else {
+    out <- exp(log_norm_const + exponent)
+  }
+  return(out)
 }
 
 #' Compute density for X of a Matrix Normal with parameters M, U and V
@@ -39,36 +39,36 @@ dmatrixnormal_plain <- function(X, M, U, V, log_p = FALSE) {
 #'
 #' @returns the density a scalar
 dmatrixnormal_schur <- function(X, M, U, V, log_p = FALSE) {
-    n <- nrow(X)
-    p <- ncol(X)
+  n <- nrow(X)
+  p <- ncol(X)
 
-    dev <- X - M
-    U_inv <- solve(U)
-    V_inv <- solve(V)
-    det_U <- det(U)
-    det_V <- det(V)
+  dev <- X - M
+  U_inv <- solve(U)
+  V_inv <- solve(V)
+  det_U <- det(U)
+  det_V <- det(V)
 
-    exponent <- -0.5 * sum((U_inv %*% dev) * (dev %*% V_inv))
+  exponent <- -0.5 * sum((U_inv %*% dev) * (dev %*% V_inv))
 
-    log_norm_const <- -0.5 * (n * p * log(2 * pi) + n * log(det_V) + p * log(det_U))
+  log_norm_const <- -0.5 * (n * p * log(2 * pi) + n * log(det_V) + p * log(det_U))
 
-    if (log_p) {
-        out <- log_norm_const + exponent
-    } else {
-        out <- exp(log_norm_const + exponent)
-    }
-    return(out)
+  if (log_p) {
+    out <- log_norm_const + exponent
+  } else {
+    out <- exp(log_norm_const + exponent)
+  }
+  return(out)
 }
 
 
 rmatrixnormal <- function(M, U, V) {
-    n <- nrow(M)
-    p <- ncol(M)
+  n <- nrow(M)
+  p <- ncol(M)
 
-    X <- matrix(rnorm(n = n * p), n, p)
-    A <- t(chol(U))
-    B <- chol(V)
+  X <- matrix(rnorm(n = n * p), n, p)
+  A <- t(chol(U))
+  B <- chol(V)
 
-    Y <- M + A %*% X %*% B
-    return(Y)
+  Y <- M + A %*% X %*% B
+  return(Y)
 }
